c779721edea37ebe4847c2eef4a37f438e4be796
MUTHANGI SRITEJA <108087209+sriteja-28@users.noreply.github.com>
2025-01-19 14:28:55 +0530

    Binded with Correct Host

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ connectMongoDB().then(() => {
   });
 
 
-  app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
+  app.listen(PORT, '0.0.0.0',() => console.log(`Server running on http://0.0.0.0:${PORT}`));
 }).catch((error) => {
   console.error("Server could not start due to database connection issues:", error);
 });
