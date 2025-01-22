779348065d91b2f33175b4972d22dabf3e213082
MUTHANGI SRITEJA <108087209+sriteja-28@users.noreply.github.com>
2025-01-22 10:17:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 const schedule = require('node-schedule');
 // const { MongoClient, ObjectId } = require('mongodb');
-const MongoClient = require('mongodb');
+const {MongoClient} = require('mongodb');
 const path = require('path');
 
 const app = express();
